68db9058f4c65ca614889d264d8a3bfddec49ea8
joebeach <jlopez.mail@gmail.com>
2014-04-19 14:46:55 -0300

    Agrego carpeta scripts

---
+++ archivos/scripts/192.168.0.45.sh
@@ -0,0 +1,11 @@
+
+				#!/bin/bash
+				statname="192.168.0.45"
+				uptime=`uptime | awk '{print $3}'`" dias, "`uptime | awk '{print $5}'`" horas"
+				statin=`/sbin/iptables -L -v -x -n| grep "192.168.0.45_i"| grep 0.0.0.0 | awk '{print $2}'`
+				statout=`/sbin/iptables -L -v -x -n| grep "192.168.0.45_o "| grep 0.0.0.0 | awk '{print $2}'`
+				echo $statin
+				echo $statout
+				echo $uptime
+				echo $statname
+				
